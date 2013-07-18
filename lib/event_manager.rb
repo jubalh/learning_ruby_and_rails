37ffde973cef77117d1b8f18b864440940d04f51
Michael Vetter <g.bluehut@gmail.com>
2013-07-18 15:09:50 +0200

    check zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,11 +1,29 @@
 require "csv"
 puts "EventManager initialized"
 
+def clean_zipcode(zip)
+  if zip.nil?
+    "0000"
+  else
+    length = zip.length
+    if length < 5
+      (5 - length).times do
+        zip = 0.to_s + zip
+      end
+      zip
+    elsif length > 5
+      zip[0..4]
+    else
+      zip
+    end
+  end
+end
+
 if File.exists? "event_attendees.csv"
   contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
   contents.each do |row|
     name = row[:first_name]
-    zip = row[:zipcode]
+    zip = clean_zipcode(row[:zipcode])
     puts "#{name} #{zip}"
   end
 end
