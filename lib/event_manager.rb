57b317bb53f74bc383399127345212b3a21b3f90
Michael Vetter <g.bluehut@gmail.com>
2013-07-18 10:57:42 +0200

    acces via header symbols. displaying zipcode

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,10 +2,11 @@ require "csv"
 puts "EventManager initialized"
 
 if File.exists? "event_attendees.csv"
-  contents = CSV.open "event_attendees.csv", headers: true
+  contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
   contents.each do |row|
-    name = row[2]
-    puts name
+    name = row[:first_name]
+    zip = row[:zipcode]
+    puts "#{name} #{zip}"
   end
 end
 
