edd789f026e330fa7c1b7d9d49dcbd6f560ac99c
Michael Vetter <g.bluehut@gmail.com>
2013-07-18 10:49:33 +0200

    omit description line

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,8 +2,11 @@ puts "EventManager initialized"
 
 if File.exists? "event_attendees.csv"
   lines = File.readlines "event_attendees.csv"
-  lines.each do |line|
-    puts line
+  lines.each_with_index do |line,index|
+    next if index == 0
+    columns = line.split(",")
+    name = columns[2]
+    puts name
   end
 end
 
