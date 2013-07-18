a0227b9644fa508d39be4d1a3e63899b8fdf08be
Michael Vetter <g.bluehut@gmail.com>
2013-07-18 10:40:21 +0200

    dont show all content

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,13 +1,9 @@
 puts "EventManager initialized"
 
 if File.exists? "event_attendees.csv"
-  contents = File.read "event_attendees.csv"
-  #puts contents
-
   lines = File.readlines "event_attendees.csv"
   lines.each do |line|
     puts line
   end
-  puts lines.methods
 end
 
