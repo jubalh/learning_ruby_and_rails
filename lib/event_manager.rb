dd711bd0c203b3f61d5fbaf63c339e52469c3574
Michael Vetter <g.bluehut@gmail.com>
2013-07-23 14:32:40 +0200

    packing creating of files in function

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -9,33 +9,35 @@ def clean_zipcode(zip)
 end
 
 def legislators_by_zipcode(zip)
-    Sunlight::Congress::Legislator.by_zipcode(zip)
+  Sunlight::Congress::Legislator.by_zipcode(zip)
+end
+
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir("output") unless Dir.exists? "output"
+  filename = "output/thanks_#{id}.html"
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 end
 
 puts "EventManager initialized"
 
 if File.exists? "form_letter.erb"
   if File.exists? "event_attendees.csv"
-   contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+    contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 
-  template_letter = File.read "form_letter.erb"
-  erb_template = ERB.new template_letter
+    template_letter = File.read "form_letter.erb"
+    erb_template = ERB.new template_letter
 
-  Dir.mkdir("output") unless Dir.exists? "output"
+    contents.each do |row|
+      name = row[:first_name]
+      id = row[0]
+      zip = clean_zipcode(row[:zipcode])
 
-   contents.each do |row|
-     name = row[:first_name]
-     id = row[0]
-     zip = clean_zipcode(row[:zipcode])
+      legislators = legislators_by_zipcode(zip)
 
-     legislators = legislators_by_zipcode(zip)
-
-     form_letter = erb_template.result(binding)
-
-     filename = "output/thanks_#{id}.html"
-     File.open(filename, 'w') do |file|
-       file.puts form_letter
-     end
-   end
+      form_letter = erb_template.result(binding)
+      save_thank_you_letter(id,form_letter)
+    end
   end
 end
